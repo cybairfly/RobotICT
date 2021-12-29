83e4c2a116b15de60f75115296b2d6674cef4db7
Tobey <cyberfly@email.cz>
2021-12-29 17:23:58 +0100

    refactor layout to components

--- Developer-Interview-Exercise/imports/ui/App.jsx
+++ Developer-Interview-Exercise/imports/ui/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import { Table } from './Table.jsx';
+import { Header } from './Header.jsx';
 
 export const App = () => (
     <div>
-        <Table/>
+        <div className="grid">
+            <header className="header">
+                <div>
+                    <Header />
+                </div>
+            </header>
+            <aside className="input">
+                <div>
+                    Left
+                </div>
+            </aside>
+            <article className="article">
+                <div id="content">
+                    <Table />
+                </div>
+            </article>
+            <aside className="right">
+                <div>
+                    Right
+                </div>
+            </aside>
+            <footer className="footer">
+                <div>
+                    <p>Footer</p>
+                </div>
+            </footer>
+        </div>
     </div>
 );
---
+++ Developer-Interview-Exercise/imports/ui/Header.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <h1 className="title">Header</h1>
+        </div>
+    );
+};
